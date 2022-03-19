b94ee4d7dc6ddfea8a5d7950aa2ed9a6e555c093
bgremmen <bas@luton.nl>
2022-03-19 17:00:34 +0100

    more working

--- product_service/app/main.py
+++ product_service/app/main.py
@@ -21,6 +21,14 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/products/{product_id}", response_model=schemas.Product)
+def read_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = crud.get_product(db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
+
+
 @app.post("/products/", response_model=schemas.Product)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
     return crud.create_product(db=db, product=product)
@@ -30,8 +38,3 @@ def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)
 def read_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     products = crud.get_products(db, skip=skip, limit=limit)
     return products
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
