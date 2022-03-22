7c2eb8f253021c67cb9ff95cc217360e71e4c514
Bas Gremmen <bas@luton.nl>
2022-03-22 19:56:35 +0100

    Working db integration

--- courierservice/daos/store_dao.py
+++ courierservice/daos/store_dao.py
@@ -8,8 +8,8 @@ class Store(Base):
     __tablename__ = "stores"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
+    name = Column(String(255))
+    description = Column(String(255))
     latitude = Column(Float)
     longitude = Column(Float)
 
