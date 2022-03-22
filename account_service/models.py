885f2e98b7cfcc5fa11f2022cb3a17bff2a36935
dylan.vanwonderen <dylan.vanwonderen@justeattakeaway.com>
2022-03-22 11:14:10 +0100

    add needed models for calls

--- account_service/models.py
+++ account_service/models.py
@@ -9,3 +9,21 @@ class UserIn(BaseModel):
     city_name: str
     country_name: str
 
+class UserOut(BaseModel):
+    id: str
+    full_name: str
+    email: str
+    street_name: str
+    street_number: int
+    city_name: str
+    country_name: str
+
+class Coordinates(BaseModel):
+    lat: str
+    long: str
+
+class Address(BaseModel):
+    street_name: str
+    street_number: int
+    city_name: str
+    country_name: str
