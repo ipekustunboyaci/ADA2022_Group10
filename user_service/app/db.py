57c178a6df8b8ab4da40596aec4c69d850343de2
dylan.vanwonderen <dylan.vanwonderen@justeattakeaway.com>
2022-03-22 15:17:26 +0100

    Revert "Revert "Merge branch 'accounts' into main""
    This reverts commit 62383b99481a27d2090bd95bfbd97d583d902825.

---
+++ user_service/app/db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import (Column, Integer, MetaData, String, Table,
+                        create_engine, ARRAY)
+
+from databases import Database
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('full_name', String(50)),
+    Column('email', String(50)),
+    Column('street_name', String(250)),
+    Column('street_number', Integer),
+    Column('city_name', String(50)),
+    Column('country_name', String(50)),
+)
+
+database = Database(DATABASE_URL)
