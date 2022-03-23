aa441f169634e46f849bce2dac81bddac3882fd7
dylan.vanwonderen <dylan.vanwonderen@justeattakeaway.com>
2022-03-23 14:34:23 +0100

    Search stores and return the id of the closest

--- faas/faas_function.py
+++ faas/faas_function.py
@@ -11,15 +11,21 @@ def calc_store_distance(request):
         client = bigquery.Client()
 
         query_job = client.query(query)
-        store_info = [{'store_id':row['store_id'], 'lat':row['lat'], 'lon':row['lon']}for row in query_job]
-        return store_info
+        stores_info_list = [{'store_id':row['store_id'], 'lat':row['lat'], 'lon':row['lon']}for row in query_job]
+        return stores_info_list
 
-    store_info = get_store_distances()
+    stores_info_list = get_store_distances()
     request_json = request.get_json()
     user_lat = request_json['lat']
     user_lon = request_json['lon']
     
     user_coordinates = (user_lat, user_lon)
-    store_coordinates = (store_info[0]['lat'], store_info[0]['lon'])
-    result = distance.distance(user_coordinates, store_coordinates).km
-    return result
+    shortest_distance = None
+    closest_store = None
+    for store_info in stores_info_list:
+        store_coordinates = (store_info['lat'], store_info['lon'])
+        current_distance = distance.distance(user_coordinates, store_coordinates).km
+        if(not shortest_distance or shortest_distance > current_distance):
+            shortest_distance = current_distance
+            closest_store = store_info['store_id']
+    return str(closest_store)
